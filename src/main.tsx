809e9df41045b7b94692aa15e471b2b2b369365c
Yuki xD <pxnditxyr@gmail.com>
2022-07-17 01:41:41 -0400

    style: Added tailwindcss and configurated

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import { GiffyMe } from './giffy-me/GiffyMe';
 
+import './index.css';
+
 createRoot( document.getElementById( 'root' )! ).render(
     <StrictMode>
         <GiffyMe />
